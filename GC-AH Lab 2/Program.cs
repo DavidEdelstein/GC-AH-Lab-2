ecf3fbe421355b82aa83b0b3bd277fceed9bd6bd
David Edelstein <David Edelstein>
2018-10-14 20:21:24 -0400

    gcah lab2 first commit

---
+++ GC-AH Lab 2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC_AH_Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                Console.Write("Please enter the WIDTH of the room:");
+                if (decimal.TryParse(Console.ReadLine(), out decimal width))
+                {
+                    Console.Write("Please enter the LENGTH of the room:");
+                    if (decimal.TryParse(Console.ReadLine(), out decimal length))
+                    {
+
+                        Console.WriteLine("Area:" + (width * length));
+                        Console.WriteLine("Perimeter:" + 2 * (length + width));
+
+
+                       
+                            Console.WriteLine("Enter 1 to continue or 2 to stop:");
+                            var answer = Console.ReadLine();
+                            if(answer == "1")
+                            {
+                              continue;
+                            }
+                            else if (answer == "2")
+                            {
+                              break;
+                            }
+                        
+
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter valid inputs.");
+                    }
+                  
+                }
+            
+
+            }
+
+
+        }
+
+    }
+}
+    
+
